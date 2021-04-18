4a467514160ff7283cac0560b039c12188756ceb
testymctestface-netizen <testymctestface@123mail.org>
2021-04-18 17:17:42 +1000

    initial commit

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,30 @@
+export class Article {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  // domain() is a utility funtion that extracts the
+  // domain from a URL
+  domain(): string {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (err) {
+      return '';
+    }
+  }
+}
